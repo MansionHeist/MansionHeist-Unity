5bbba04f3efe4c4a62c94c0ba6238bf92287b742
Hyemin Lee <cotloz88@gmail.com>
2023-07-22 01:00:50 +0900

    feat: 참여 멤버 출력 기능 추가

---
+++ Assets/Scripts/UI/RoomSceneUI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSceneUI : MonoBehaviour
+{
+    public void OnClickReadyButton()
+    {
+        Debug.Log("Ready Button Clicked");
+    }
+
+    public void OnClickStartButton()
+    {
+        Debug.Log("Start Button Clicked");
+    }
+}
