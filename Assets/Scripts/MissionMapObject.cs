1f8d5444ffe62db8a638fb1c9f8fd9ae1a501f9d
rlagk1012 <rlagkwls1012@gmail.com>
2023-07-24 15:18:17 +0900

    wip

---
+++ Assets/Scripts/MissionMapObject.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionMapObject : MonoBehaviour
+{
+    protected SpriteRenderer spriteRenderer;
+    private bool isClickable = false;
+
+    [SerializeField]
+    private Color clickableColor;
+
+    [SerializeField]
+    GameObject missionUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    protected void OnTriggerEnter2D(Collider2D collision)
+    {
+        var player = collision.GetComponent<PlayerController>();
+        if (player != null)
+        {
+            spriteRenderer.color = clickableColor;
+            isClickable = true;
+        }
+    }
+
+    protected void OnTriggerExit2D(Collider2D collision)
+    {
+        var player = collision.GetComponent <PlayerController>();
+        if(player != null)
+        {
+            spriteRenderer.color = Color.white;
+            isClickable = false;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if(isClickable)
+        {
+            missionUI.SetActive(true);
+        }
+    }
+}
