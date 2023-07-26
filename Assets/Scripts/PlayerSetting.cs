b57f23f8dc57dfa83a2a8e19e8589d5d0c5596fd
Hyemin Lee <cotloz88@gmail.com>
2023-07-26 16:26:17 +0900

    fix: merge error and deactivate some UIs

--- Assets/Scripts/PlayerSetting.cs
+++ Assets/Scripts/PlayerSetting.cs
@@ -13,5 +13,5 @@ public class PlayerSettings
     public static string userName;
 
     // TODO: userType 서버로부터 받아와서 설정해줘야함
-    public static EPlayerType userType = EPlayerType.Guard;
+    public static EPlayerType userType = EPlayerType.Thief;
 }
